0e089842c60e05f631485f0bbf9f80a7855bdab5
xelva <xelvacodes@gmail.com>
2022-04-22 13:52:47 -0500

    getting started w jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// create a react component
+const App = () => {
+    return <div>Hi there!</div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// take the react component and show it on the screen
+ReactDOM.render(
+    <App />, document.querySelector('#root')
+)
