9b861517a5b0920f1c89151376c4510fb54736c4
xelva <xelvacodes@gmail.com>
2022-04-22 18:46:50 -0500

    jsx intro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 // import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
+
+const testMe = () => {
+    return 'blah!'
+}
 // create a react component
 const App = () => {
-    return <div>Hi there!</div>
+    const buttonText = testMe();
+    return (
+        <div>
+        <label className="label" htmlFor="name">Enter Name</label>
+        <input id="name"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {buttonText}
+        </button>
+    </div>
+    );
 }
 
 // take the react component and show it on the screen
-ReactDOM.render(
-    <App />, document.querySelector('#root')
-)
+root.render( <App /> )
